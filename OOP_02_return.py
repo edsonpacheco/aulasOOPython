1b5914ed3e9ef5a58ee2ff9f6562a97e31c74dbe
edsonpacheco <128686857+edsonpacheco@users.noreply.github.com>
2026-01-11 15:54:25 -0300

    Create OOP_02_return.py

---
+++ OOP_02_return.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    return a + b  # Retorna resultado
+
+def saudacao(nome):
+    print(f"Olá, {nome}")
+    # Sem return: retorna None
+
+res = soma(5, 3)     # res = 8
+val = saudacao("UFPR") # val = None
