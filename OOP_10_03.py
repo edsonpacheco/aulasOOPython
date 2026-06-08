95b2306aaee83ce7e174819e71be1bdf5e374657
edsonpacheco <128686857+edsonpacheco@users.noreply.github.com>
2026-06-08 10:18:22 -0300

    Add data download function for UCI dataset

--- OOP_10_03.py
+++ OOP_10_03.py
@@ -3,6 +3,23 @@ import numpy as np
 import os
 import multiprocessing
 
+import requests
+import zipfile
+import io
+
+# Endereço da base de dados de consumo da UCI
+url = "https://archive.ics.uci.edu/static/public/235/individual+household+electric+power+consumption.zip"
+
+def baixar_dados(url_fonte):
+    print("Iniciando download...")
+    resposta = requests.get(url_fonte)  
+    # O objeto 'resposta' contem o binario do arquivo
+    com_zip = zipfile.ZipFile(io.BytesIO(resposta.content))
+    com_zip.extractall("./dados_smart_grid")
+    print("Dados extraídos com sucesso!")
+# Execução
+
+    
 def pipeline_diagnostico(df_set):
     """
     Executa a engenharia de recursos e avalia os alertas de forma vetorizada.
@@ -18,7 +35,9 @@ def pipeline_diagnostico(df_set):
     return alertas
 
 if __name__ == "__main__":
-    print("Sistema de Monitoramento Distribuído UFPR - v2")
+    baixar_dados(url)
+
+    print("Sistema de Monitoramento Distribuído UFPR - v3")
     cores = os.cpu_count()
     print(f"Cores detectados: {cores}")
 
